afac579be9ce1f8ca7da0e06e1744da65fe01ad6
terraelfi <123456789+elfishahrin@gmail.com>
2024-11-24 21:44:54 +0800

    03 Exercise done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // split = divide string to array
+    // reverse = reverse array
+    // join = combine string to new string
+    const result = string.split('').reverse().join('');
+    return result;
 };
 
 // Do not edit below this line
