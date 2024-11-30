89013f90c3cdd85a34be6c3d4bf5a0004d07a2c8
terraelfi <123456789+elfishahrin@gmail.com>
2024-11-30 22:36:33 +0800

    05 Exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    let number = 0;
+    if (min < 0 || max < 0) return "ERROR";
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
+
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+      }
+
+
+    for (let i = min; i <= max; i++) {
+        number += i;
+    }
+    return number;
 };
 
 // Do not edit below this line
