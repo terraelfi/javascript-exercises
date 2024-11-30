9b231f9f3b80269cb3a267894e560bb6f04fe9f9
terraelfi <123456789+elfishahrin@gmail.com>
2024-11-30 21:10:42 +0800

    04 Exercise done (VERY HARD)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(passedArray, ...args) {
+    const newArray = []
+    passedArray.forEach(items => {
+        if (!args.includes(items)) {
+            newArray.push(items)
+        }
+    });
+    return newArray
 };
+
+console.log(removeFromArray([1,2,3,4],1,2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
