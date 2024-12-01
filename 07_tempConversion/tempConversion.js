28333f54463d61cf19ff59ea09363d53a4993903
terraelfi <123456789+elfishahrin@gmail.com>
2024-12-01 21:53:03 +0800

    07 Exercise Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const convertfahrenheit = (f - 32) * 5 / 9;
+  const fahrenheitRounded = Math.round(convertfahrenheit * 10) / 10;
+
+  console.log(fahrenheitRounded);
+  return fahrenheitRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const convertCelcius = c * 9/5 + 32;
+  const celciusRounded = Math.round(convertCelcius * 10) / 10;
+
+  console.log(celciusRounded);
+  return celciusRounded;
 };
+
+convertToCelsius(32);
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
