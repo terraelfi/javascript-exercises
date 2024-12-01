30b841000ae45aab3f3f1c23ded76ff6ed67b052
terraelfi <123456789+elfishahrin@gmail.com>
2024-12-01 21:27:01 +0800

    06 Exercise Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(userYears) {
+    const isYearDivisiblebyFour = userYears % 4 === 0;
+    const isYearDivisiblebyFourHundred = userYears % 400 === 0;
+    const isCentury = userYears % 100 === 0;
 
+    if (isYearDivisiblebyFour && (!isCentury || isYearDivisiblebyFourHundred)) {
+        return true
+    } else {
+        return false
+    }
 };
+
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
